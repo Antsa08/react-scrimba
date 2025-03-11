2f795e29d57ed6875a1e0d42e5fef803695c69ba
antsa <antsanirina@odity.fr>
2025-03-11 15:15:13 +0300

    Fetching data from a js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-
 import Jokes from './components/Jokes'
+
+import jokesData from './assets/jokesData.js'
 
 function App() {
 
-  console.log('test');
+  const jokesComponent = jokesData.map(function(joke) {
+    return (<Jokes setup={joke.setup} punchline={joke.punchline} />)
+  });
 
   return (
     <>
-      <Jokes
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Jokes
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-      <Jokes
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Jokes
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-      />
-      <Jokes
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
+      { jokesComponent }
     </>
   )
 }
