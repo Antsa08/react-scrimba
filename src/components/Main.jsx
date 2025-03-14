ddec371ad0add4a7d4fdec5225468f9fa8e516b1
antsa <antsanirina@odity.fr>
2025-03-14 15:46:23 +0300

    Templates - components meme generator

---
+++ src/components/Main.jsx
@@ -0,0 +1,29 @@
+export default function Main({ image, top, bottom }) {
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={ image } />
+                <span className="top">{ top }</span>
+                <span className="bottom">{ bottom }</span>
+            </div>
+        </main>
+    )
+}
