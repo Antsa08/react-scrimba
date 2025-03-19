47a525cc244b25d25b353a0804e5174b5838d9bc
antsa <antsanirina@odity.fr>
2025-03-19 17:51:10 +0300

    Update using map function

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -3,8 +3,10 @@ import styles from './generate.module.scss'
 export const Generate = () => {
     return (
         <section className={ styles.recipe }>
-            <p>Ready for a recipe?</p>
+            <div>
+                <p>Ready for a recipe?</p>
             <span>Generate a recipe from you list of ingredients</span>
+            </div>
             <button type="text">Get a recipe</button>
         </section>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,29 @@ import { Generate } from './Generate'
 import styles from './main.module.scss'
 
 export const Main = () => {
+
+	const ingredients = ['Chicken', 'Oregano', 'Tomatoes'];
+	const ingredientList = ingredients.map((ingredient) => {
+		return (<li>{ingredient}</li>);
+	});
+
+	function handleClick() {
+		alert('you clicked');
+	}
+
   return (
     <main>
     	<section className={ styles.input }>
     		<input type="text" placeholder="e.g. oregano" />
-    		<button type="button">+ Add ingredient</button>
+    		<button type="button" onClick={ handleClick }>+ Add ingredient</button>
     	</section>
 		<section className={ styles.ingredients }>
 			<p className={ styles.title }>Ingredients on hand:</p>
-			<ul className={ styles.list }>
+
+			<ul>
+				{ ingredientList }
+			</ul>
+			{/*<ul className={ styles.list }>
 				<li>Chicken breasts</li>
 				<li>Most of the main spices</li>
 				<li>Olive oil</li>
@@ -21,7 +35,7 @@ export const Main = () => {
 				<li>Chicken broth</li>
 				<li>Parmesan cheese</li>
 				<li>Spinach</li>
-			</ul>
+			</ul>*/}
 		</section>
 		<Generate />
 	</main>
