7e52e32082353f091049002bf64dacd9428efd9e
antsa <antsanirina@odity.fr>
2025-03-31 15:44:30 +0300

    Update using state and conditional rendering

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,9 +1,26 @@
+import React from 'react'
+
 function Jokes(props) {
 	console.log(props);
+
+	const [isShown, setIsShown] = React.useState(false)
+
+	function toggle() {
+		setIsShown(prev => prev = !isShown)
+		console.log(isShown)
+	}
+
 	return (
 		<div>
-			<p>{ props.setup }</p>
-			<p>{ props.punchline }</p>
+			{ isShown ?
+				<div>
+					<p>{ props.setup }</p>
+					<p>{ props.punchline }</p>
+				</div>
+			 :
+				<p><b>Rien à afficher pour le moment</b></p>
+			}
+			<button onClick={ toggle }>{ isShown ? 'Hide answer' : 'Show answer' }</button>
 			<hr />
 		</div>
 	)
