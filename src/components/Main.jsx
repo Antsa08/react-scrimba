4809a457583f86a975f49cc7ae53364a169f51ba
antsa <antsanirina@odity.fr>
2025-05-27 14:49:01 +0300

    Use effect for memes generator

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,52 @@
+import React from 'react'
+
 export default function Main({ image, top, bottom }) {
+
+    const [memes, setMemes] = React.useState({
+        topText: 'One does not simply',
+        bottomText: 'Walk into Mordor',
+        imageUrl: image
+    });
+
+    const [count, setCount] = React.useState(0);
+
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    React.useEffect(function() {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(response => response.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    async function setMeme() {
+        const integerFromRandom = Math.floor(Math.random() * await allMemes.length)
+        setMemes(function(prevMeme) {
+            return {
+                ...prevMeme,
+                imageUrl: allMemes[integerFromRandom].url
+            }
+        })
+    }
+
+    function loadMemes(e) {
+        let { name, value } = e.currentTarget;
+
+        setMemes(prevMemes => ({
+            ...prevMemes,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="One does not simply"
+                        placeholder="ex: One does not simply"
                         name="topText"
+                        value={ memes.topText }
+                        onChange={ loadMemes }
                     />
                 </label>
 
@@ -15,14 +55,16 @@ export default function Main({ image, top, bottom }) {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        value={ memes.bottomText }
+                        onChange={ loadMemes }
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={ setMeme }>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src={ image } />
-                <span className="top">{ top }</span>
-                <span className="bottom">{ bottom }</span>
+                <img src={ memes.imageUrl } />
+                <span className="top">{ memes.topText }</span>
+                <span className="bottom">{ memes.bottomText }</span>
             </div>
         </main>
     )
